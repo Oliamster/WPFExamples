d36c8cb6a3e7e2726421bbd99374592571e5edfd
Oliamster <oliamster@gmail.com>
2020-04-28 00:42:41 +0200

    added the project binding DP to clr object

---
+++ WPFExamples/BindingDPToClrProperty/Section.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingDPToClrProperty
+{
+    /// <summary>
+    /// Interaction logic for CostumUserControl.xaml
+    /// </summary>
+    public partial class Section : UserControl
+    {
+        public Section()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string SectionFontSize { get; set; }
+        public string SectionTitle { get; set; }
+    }
+}
